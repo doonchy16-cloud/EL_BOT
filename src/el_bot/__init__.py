b00206c3e5fc9effb1e05e12cc5e1293b2fa2322
doonchy16-cloud <doonchy16@gmail.com>
2026-08-11 11:56:42 -0700

    Export EL Intelligence Engine from package

--- src/el_bot/__init__.py
+++ src/el_bot/__init__.py
@@ -1,6 +1,7 @@
 """EL Bot package."""
 
 from .core.engine import CoreEngine, ParseResult, Token, TokenKind, ValidationStatus
+from .intelligence import IntelligenceEngine, InterpretationResult, InterpretedLine, Relationship
 from .vocabulary import CANONICAL_SYMBOLS, SymbolAuthority, VocabularyEngine, VocabularyResult
 
 __all__ = [
@@ -13,4 +14,8 @@ __all__ = [
     "SymbolAuthority",
     "VocabularyEngine",
     "VocabularyResult",
+    "IntelligenceEngine",
+    "InterpretationResult",
+    "InterpretedLine",
+    "Relationship",
 ]
