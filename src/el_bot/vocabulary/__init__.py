aac1955c0b781c0a5845530a854a2014c39c5bb1
doonchy16-cloud <doonchy16@gmail.com>
2026-08-11 11:53:44 -0700

    Expose Vocabulary Authority Engine

---
+++ src/el_bot/vocabulary/__init__.py
@@ -0,0 +1,10 @@
+"""Canonical EL vocabulary authority engine."""
+
+from .authority import CANONICAL_SYMBOLS, SymbolAuthority, VocabularyEngine, VocabularyResult
+
+__all__ = [
+    "CANONICAL_SYMBOLS",
+    "SymbolAuthority",
+    "VocabularyEngine",
+    "VocabularyResult",
+]
